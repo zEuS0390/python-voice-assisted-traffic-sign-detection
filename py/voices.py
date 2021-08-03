67c4bbc9662d7795847a381a754ce92a95fe6688
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2021-08-03 21:33:45 +0800

    Add voice functionality

--- py/voices.py
+++ py/voices.py
@@ -1,9 +1,51 @@
 from py.util import join, getDir
 from py.database import trafficRecoDB
-import os, glob
+import os, glob, winsound, random
 
 root_dir = getDir(getDir(__file__))
 req_dir = join(root_dir, "req")
+
+# file_name
+# file_path
+# class_name
+# lang_name
+# gender_name
+# class_name
+
+class GroupVoice:
+
+    def __init__(self):
+        self.femaleVoices = []
+        self.maleVoices = []
+
+    def add(self, voice):
+        print(voice.file_name, voice.gender_name)
+        if voice.gender_name == "female":
+            self.femaleVoices.append(voice)
+        elif voice.gender_name == "male":
+            self.maleVoices.append(voice)
+
+    def __repr__(self):
+        return f"{self.voices}"
+
+class Voice:
+
+    def __init__(self, file_name, file_path,
+                       file_num, lang_name,
+                       gender_name, class_name):
+        self.file_name = file_name
+        self.file_path = file_path
+        self.file_num = file_num
+        self.class_name = class_name
+        self.lang_name = lang_name
+        self.gender_name = gender_name
+
+    def play(self):
+        winsound.PlaySound(self.file_path, winsound.SND_FILENAME)
+
+    def __repr__(self):
+        return self.file_name
+
 
 class Voices(trafficRecoDB):
 
@@ -15,11 +57,30 @@ class Voices(trafficRecoDB):
         self.removeNoExist("voice_files")
         self.insertToDB()
 
+    def playVoice(self, class_name, gender_name):
+        if gender_name == "male":
+            voices = self.getVoices()[class_name].maleVoices
+            random.choice(voices).play()
+        elif gender_name == "female":
+            voices = self.getVoices()[class_name].femaleVoices
+            random.choice(voices).play()
+
     # Get the voices for use
     def getVoices(self):
         voices = {}
         for name in self.names:
-            voices[name] = self.getAllWhere("voice_files", class_Name=name)
+            class_voices = self.getAllWhere("voice_files", class_Name=name)
+            group_c_voices = GroupVoice()
+            for c_voice in class_voices:
+                file_name = c_voice[0]
+                file_path = c_voice[1]
+                file_num = c_voice[2]
+                lang_name = c_voice[3]
+                gender_name = c_voice[4]
+                class_name = c_voice[5]
+                group_c_voices.add(Voice(file_name, file_path, file_num,
+                                         lang_name, gender_name, class_name))
+            voices[name] = group_c_voices
         return voices
 
     # Set up the requirements
