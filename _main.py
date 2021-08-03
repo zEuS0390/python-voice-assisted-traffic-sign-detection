d646ee40bb150ce994992b2ed7001427602eef2d
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2021-08-03 18:57:55 +0800

    Rearrange

--- _main.py
+++ _main.py
@@ -3,7 +3,7 @@ from py.imagesInput import ImagesInput
 from py.cameraInput import CameraInput
 from py.voices import Voices
 from py.util import getDir, join
-import argparse, configparser
+import argparse, configparser, sys
 
 """
     TECHNOLOGICAL INSTITUTE OF THE PHILIPPINES - QUEZON CITY
@@ -21,21 +21,22 @@ if __name__=="__main__":
     config = configparser.ConfigParser()
     config_file_path = join(root_dir, "config.ini")
     config.read(config_file_path)
-    audioDB = Voices(config)
-    camInput = CameraInput(config)
-    vidInput = VideoInput(config)
-    imgInput = ImagesInput(config)
     parser = argparse.ArgumentParser()
     parser.add_argument("-cam", "--camera", action="store_true", help="Detect using internal/external camera")
     parser.add_argument("-ipcam", "--ipcamera", action="store", help="Detect using ip camera")
     parser.add_argument("-vidin", "--videoinput", action="store", help="Detect in premade video")
     parser.add_argument("-imgin", "--imginput", action="store", help="Detect all in directory")
-    args = parser.parse_args()
-    if args.camera:
-        camInput.detectCamera()
-    elif args.ipcamera is not None:
-        camInput.detectIPCamera(args.ipcamera)
-    elif args.videoinput is not None:
-        vidInput.detectVideo(args.videoinput)
-    elif args.imginput is not None:
-        imgInput.detectImages(args.imginput)
+    if len(sys.argv) > 1:
+        args = parser.parse_args()
+        if args.camera:
+            camInput = CameraInput(config)
+            camInput.detectCamera()
+        elif args.ipcamera is not None:
+            camInput = CameraInput(config)
+            camInput.detectIPCamera(args.ipcamera)
+        elif args.videoinput is not None:
+            vidInput = VideoInput(config)
+            vidInput.detectVideo(args.videoinput)
+        elif args.imginput is not None:
+            imgInput = ImagesInput(config)
+            imgInput.detectImages(args.imginput)
