be5b96a652cadc29242c20523db457e93798ee53
zEuS-0390 <zeusjamesbaltazar@gmail.com>
2021-08-03 17:49:09 +0800

    Import module TrafficSignRecognition and functions

--- py/videoInput.py
+++ py/videoInput.py
@@ -1,23 +1,30 @@
-import cv2, os, py.detector
+from os.path import join, dirname, exists, basename, realpath
+from py.detector import TrafficSignRecognition
+from os import mkdir
+import cv2
 
-class VideoInput(py.detector.TrafficSignRecognition):
+class VideoInput(TrafficSignRecognition):
 
     def __init__(self, conf):
         super(VideoInput, self).__init__(conf)
 
     # Detect in frames from video input
     def detectVideo(self, file_path):
-        newDir = os.path.join(os.path.dirname(file_path), "detected_videos")
-        if not os.path.exists(newDir):
-            os.mkdir(newDir)
-        capVid = cv2.VideoCapture(file_path)
+        newDir = join(dirname(file_path), "detected_videos")
+        if not exists(newDir):
+            mkdir(newDir)
+        if not exists(realpath(file_path)):
+            print(f"[ERROR]: {basename(file_path)} does not exist")
+            return
+        capVid = cv2.VideoCapture(realpath(file_path))
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
         curr_frame, total_frames = 0, int(capVid.get(cv2.CAP_PROP_FRAME_COUNT))
-        vidWriter = cv2.VideoWriter(os.path.join(newDir, os.path.basename(file_path)),
+        vidWriter = cv2.VideoWriter(join(newDir, basename(file_path)),
                                     fourcc,
                                     capVid.get(cv2.CAP_PROP_FPS),
                                     (int(capVid.get(cv2.CAP_PROP_FRAME_WIDTH)),
                                      int(capVid.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+        prompt = "[VIDEO SAVED]: Traffic sign detection is done."
         try:
             while capVid.isOpened():
                 curr_frame += 1
